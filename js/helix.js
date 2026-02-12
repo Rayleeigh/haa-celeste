abd8f210a89af829e7f7ce5f49a0b594d34f5b5e
Rayleeigh <125958687+Rayleeigh@users.noreply.github.com>
2026-02-13 00:10:37 +0100

    release: minor v2.2.0

---
+++ js/helix.js
@@ -0,0 +1,24 @@
+(() => {
+  const helix = document.getElementById("dna-helix");
+  if (!helix) return;
+
+  const RUNGS = 30;
+
+  for (let i = 0; i < RUNGS; i++) {
+    const rung = document.createElement("div");
+    rung.className = "dna-rung";
+    rung.style.setProperty("--i", i);
+    helix.appendChild(rung);
+  }
+
+  let time = 0;
+
+  const tick = () => {
+    time += 0.2;
+    const scroll = window.scrollY * 0.25;
+    helix.style.setProperty("--offset", time + scroll);
+    requestAnimationFrame(tick);
+  };
+
+  requestAnimationFrame(tick);
+})();
